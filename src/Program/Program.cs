d332e704ddab043240bb1afb43128790a66eb658
NicorRodriguez <poptnico75@gmail.com>
2022-11-14 18:40:44 -0300

    Finiched tarea visitor

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Library;
 
 namespace Program
@@ -7,13 +8,16 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            List<Person> myPersons = new List<Person>();
+            Person n1 = new Person(1, "a");
+            Person n2 = new Person(2, "a");
+            Person n3 = new Person(3, "a");
+            Person n4 = new Person(4, "NicoCrack");
+            Person n5 = new Person(5, "a");
+            Person n6 = new Person(6, "a");
+            Person n7 = new Person(7, "aaa");
+
+            myPersons.Add(n1);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -24,7 +28,9 @@ namespace Program
             n3.AddChildren(n6);
             n3.AddChildren(n7);
 
-            // visitar el árbol aquí
+            Console.WriteLine(Visitor.Instance.countChilds(myPersons.AsReadOnly()));
+            Console.WriteLine(VisitorMax.Instance.getMax(myPersons.AsReadOnly()));
+            Console.WriteLine(VisitorName.Instance.getLonger(myPersons.AsReadOnly()));
         }
     }
 }
